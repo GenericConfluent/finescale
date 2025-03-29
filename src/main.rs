16d7e2e7b60c2aed3d0eea0bc0363b4e3983d270
GenericConfluent <m13power@gmail.com>
2025-03-28 18:42:32 -0600

    refactor: migrate some `iced` stuff
    Build is currently failing. Which is expected cause the changes here are
    half complete.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,3 @@
-use iced::{window, Application, Settings};
-
 fn main() -> iced::Result {
-    finescale::FinescaleApp::run(Settings {
-        id: Some("finescale".into()),
-        window: window::Settings {
-            size: iced::Size::new(800.0, 400.0),
-            visible: true,
-            decorations: true,
-            ..Default::default()
-        },
-        antialiasing: true,
-        ..Default::default()
-    })
+    finescale::ui::run()
 }
